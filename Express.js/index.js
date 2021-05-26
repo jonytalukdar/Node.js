884363b7b514da128ca061a4f197828e20ae76bd
jonytalukdar <jonytalukdar30@gmail.com>
2021-05-26 18:13:19 +0600

    added multiple route

--- Express.js/index.js
+++ Express.js/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const app = express();
+
+app.get('/about', (req, res) => {
+  res.send('this is about page');
+});
+
+app.get('/help', (req, res) => {
+  res.send('this is help page');
+});
 
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+app.get('*', (req, res) => {
+  res.send('404 page not found');
 });
 
 const PORT = process.env.PORT || 1200;
