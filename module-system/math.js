996639635aad2cd7a59486b6def26d8b3afc2122
jonytalukdar <jonytalukdar30@gmail.com>
2021-05-26 12:32:56 +0600

    completed import system

--- module-system/math.js
+++ module-system/math.js
@@ -4,11 +4,11 @@ const div = (a, b) => a / b;
 
 const test = (a, b) => (a, b) / (a, b);
 
-// module.exports = {
-//   test,
-//   add,
-//   sub,
-// };
+module.exports = {
+  test,
+  add,
+  sub,
+};
 
 module.exports.test = test;
 
